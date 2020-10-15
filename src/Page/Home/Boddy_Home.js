8dd032d4a7b33b9a6cb832bd97025dd8d317b98a
algrosset-grange <agrosset@student.42.fr>
2020-10-15 15:19:56 +0200

    2 files modified

--- src/Page/Home/Boddy_Home.js
+++ src/Page/Home/Boddy_Home.js
@@ -1,38 +1,35 @@
 import React from "react";
-import {Header_Part} from "../Header_Part.js";
-import {Footer_Part} from "../Footer_Part.js";
-export {Boddy_Home};
+import { Header_Part } from "../Header_Part.js";
+import { Footer_Part } from "../Footer_Part.js";
+export { Boddy_Home };
 import App_navigation from "../Nav-Bar.js";
 
-
 const aaa = "here is a constant out of the class";
-const nav = App_navigation;
+const nav = { App_navigation };
 
-class Boddy_Home extends React.Component{
-
+class Boddy_Home extends React.Component {
   constructor(props) {
     super(props);
     //this.state = { name: 'Frarthur' };
     //this.changeName = this.changeName.bind(this);
   }
 
-    ///<Header />
+  ///<Header />
 
   render() {
     return (
-    <React.Fragment>
-      <Header_Part />
-      <br/>
-      {aaa}
-      <br/>
-      <App_navigation />
-      <br/>
-      the Boddy_Homes
-      <br/>
-      <br/>
-      <Footer_Part />
-    </React.Fragment>
-  );
+      <React.Fragment>
+        <Header_Part />
+        <br />
+        {aaa}
+        <br />
+        <App_navigation />
+        <br />
+        the Boddy_Homes
+        <br />
+        <br />
+        <Footer_Part />
+      </React.Fragment>
+    );
   }
-
 }
--- src/Page/Nav-Bar.js
+++ src/Page/Nav-Bar.js
@@ -1,14 +1,8 @@
 import React from "react";
-import {Boddy_Home} from "./Home/boddy_Home.js";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Boddy_Home } from "./Home/boddy_Home.js";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default class App_navigation extends React.Component {
-  render() {
+export default function App_navigation() {
   return (
     <Router>
       <div>
@@ -42,17 +36,54 @@ export default class App_navigation extends React.Component {
       </div>
     </Router>
   );
-  }
 }
 
+/*class App_navigation extends React.Component {
+  render() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. /}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+  }
+}*/
+
 function Home() {
-  return ;
+  return "aaa";
 }
 
 function About() {
-  return ;
+  return "bbb";
 }
 
 function Users() {
-  return ;
+  return "ccc";
 }
