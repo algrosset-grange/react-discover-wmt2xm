80dd482417357ad182b0a91a881247aabb12bb0e
algrosset-grange <agrosset@student.42.fr>
2020-11-30 17:17:24 +0100

    1 file modified

--- src/component_page/RouletteEuropeen.js
+++ src/component_page/RouletteEuropeen.js
@@ -53,15 +53,54 @@ class RouletteEuropeen extends React.Component {
     return (
       <React.Fragment>
         <div>
-          La roulette européenne est composé de 37 cases de 0 à 36
-          La table présente égallement 3 couleurs : vert rouge et noir
-          Seul le  est vert, les 36 autres cases sont répartie équitablement (noté que tous les nombres pair ne sont pas rouge.)
+          La roulette européenne est composé de 37 cases de 0 à 36 La table
+          présente égallement 3 couleurs : vert rouge et noir Seul le est vert,
+          les 36 autres cases sont répartie équitablement (noté que tous les
+          nombres pair ne sont pas rouge.)
         </div>
         <div>
-        Il y a donc une case sur 36 (1/36) de tomber sur le bon numéro<br />
-        18/37 en misant pair (0 non inclus), impair, noir ou rouge<br />
+          Il y a donc une case sur 36 (1/36) de tomber sur le bon numéro, on
+          remporte 35 fois la mise
+          <br />
+          18/37 en misant pair (0 non inclus), impair, noir ou rouge, on
+          remporte 1 fois la mise.
+          <br />
+          <br />
+          <ul>
+            Mise sur un numéro (plein) → 35 fois la mise. (1 chance sur 36) soit{" "}
+            {(1 / 36) * 100}%
+          </ul>
+          <ul>
+            Mise sur deux numéros (cheval : exemple : 7-10) → 17 fois la mise.
+            (2 chance sur 36) soit {(2 / 36) * 100}%
+          </ul>
+          <ul>
+            Mise sur trois numéros (transversale dite aussi « transversale
+            pleine » : exemple : 7-8-9) → 11 fois la mise. {(3 / 36) * 100}%
+          </ul>
+          <ul>
+            Mise sur quatre numéros (carré : exemple : 25-26-28-29) → 8 fois la
+            mise. {(4 / 36) * 100}%
+          </ul>
+          <ul>
+            Mise sur six numéros (sixain dit aussi « transversale simple » :
+            exemple : 13-14-15-16-17-18) → 5 fois la mise. {(6 / 36) * 100}%
+          </ul>
+          <ul>
+            Mise sur douze numéros (douzaine ou colonne : exemple : colonne 1-34
+            ou douzaine 1-12) → 2 fois la mise. {(12 / 36) * 100}%
+          </ul>
+          <ul>
+            Mise sur une chance simple, soit dix-huit numéros (Noir-Rouge -
+            Pair-Impair - Manque-Passe) → 1 fois la mise. {(1 / 36) * 100}%
+          </ul>
+          <ul>
+            Mise sur vingt-quatre numéros (deux colonnes adjacentes,
+            vingt-quatre premiers (1-24) ou vingt-quatre derniers (13-36), ces
+            mises étant placées à cheval sur les colonnes ou les douzaines) → la
+            moitié de la mise {(24 / 36) * 100}%
+          </ul>
         </div>
-
       </React.Fragment>
     );
   }
