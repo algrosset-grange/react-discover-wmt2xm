0a4e6e62e19d850fa6c88fa99e13a568cf4b64d5
algrosset-grange <agrosset@student.42.fr>
2020-12-23 11:13:06 +0100

    1 file modified

--- src/component_page/RouletteEuropeen.js
+++ src/component_page/RouletteEuropeen.js
@@ -50,14 +50,50 @@ class RouletteEuropeen extends React.Component {
       case "1":
         return (
           <div>
-            <input type="number" id="valeurmisé-1" />
+            <input type="number" id="valeurmise-1" />
           </div>
         );
       case "2":
         return (
           <div>
-            <input type="number" id="valeurmisé-1" />
+            <input type="number" id="valeurmise-1" />
             <input type="number" id="valeurmisé-2" />
+          </div>
+        );
+      case "3":
+        return (
+          <div>
+            <input type="number" id="valeurmise-1" />
+            <input type="number" id="valeurmise-2" />
+            <input type="number" id="valeurmise-3" />
+          </div>
+        );
+      case "6":
+        return (
+          <div>
+            <input type="number" id="valeurmise-sixtain-1" /> serie 1-6
+            <input type="number" id="valeurmise-sixtain-2" /> serie 7-12
+            <input type="number" id="valeurmise-sixtain-3" /> serie 13-18
+            <input type="number" id="valeurmise-sixtain-4" /> serie 19-24
+            <input type="number" id="valeurmise-sixtain-5" /> serie 25-30
+            <input type="number" id="valeurmise-sixtain-6" /> serie 31-36
+          </div>
+        );
+      case "12":
+        return (
+          <div>
+            <input type="number" id="valeurmise-douzaine-1" /> serie 1-12
+            <input type="number" id="valeurmise-douzaine-2" /> serie 13-24
+            <input type="number" id="valeurmise-douzaine-3" /> serie 25-36
+          </div>
+        );
+      case "18":
+        return (
+          <div>
+            <input type="number" id="valeurmise-simple-pair" /> pair
+            <input type="number" id="valeurmise-simple-impair" /> impair
+            <input type="number" id="valeurmise-douzaine-rouge" /> rouge
+            <input type="number" id="valeurmise-douzaine-noir" /> noir
           </div>
         );
       default:
@@ -93,6 +129,9 @@ class RouletteEuropeen extends React.Component {
           <input type="radio" value="6" name="gender" /> sizain (6 cases)
           <br />
           <input type="radio" value="12" name="gender" /> douzaine (12 cases)
+          <br />
+          <input type="radio" value="18" name="gender" /> chance simple (18
+          cases)
           <br />
           <input type="radio" value="24" name="gender" /> Deux douzaines à
           cheval (24 cases)
